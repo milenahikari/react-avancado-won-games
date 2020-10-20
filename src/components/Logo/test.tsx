385e3fd1f88e76ce02725fcbb169426b250f2c86
Milena <milenahikari11@gmail.com>
2020-10-19 21:22:30 -0300

    Criando teste de cor no componente Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo/>)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#FAFAFA'})
+  })
+
+  it('should render a black label when color is passed', () => {
+    renderWithTheme(<Logo color="black"/>)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({color: '#030517'})
   })
 })
